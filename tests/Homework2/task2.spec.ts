622ca72a8ea9e376b1e09dcd209937497cd898b4
Ruzhica <ruzicadragomirovska@gmail.com>
2024-10-16 15:17:31 +0200

    task-2 from homework-2

---
+++ tests/Homework2/task2.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+test.describe("Test Suite for Shopping cart", () =>{
+
+
+    test.describe("Test Suite for Adding an item to the shopping cart", () =>{
+        test('test add item to the shopping cart from home page', async ({ page }) => {
+
+        });
+
+
+        test('test add item to the shopping cart from the product page', async ({ page }) => {
+
+        });
+
+    });
+
+
+    test.describe("Test Suite for Removing an item from the shopping cart", () =>{
+
+        test('test remove item to the shopping cart from home page', async ({ page }) => {
+
+        });
+
+
+        test('test add item to the shopping cart from the product page', async ({ page }) => {
+
+        });
+
+    });
+
+
+});
